ca6b50d95582dcd9ebebefced8025dcb5791a19c
zigzagzack <ziggyzaggyzack@gmail.com>
2021-01-14 10:29:32 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,13 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    // prevent the page from reloading  
+    e.preventDefault();
+    // In an event handler, $(this) refers to   
+    // the object that triggered the event  
+    $(this).css("background-color", "#7fff00");
 }
